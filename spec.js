7d318de7e3410e179207e3e1d5950d53e73edac7
wlsf82 <wlsf82@gmail.com>
2016-03-05 20:53:03 -0300

    Added new test to the suite.

--- spec.js
+++ spec.js
@@ -1,12 +1,27 @@
 describe( 'Drupal', function() {
 
-  it('right theme is set', function() {
+  it( 'right theme is set', function() {
 
     // Arrange:
     browser.get('/');
 
     // Assert:
     expect(browser.executeScript('return Drupal.settings.ajaxPageState.theme')).toEqual('bluecheese');
+  });
+
+  it( 'try to login without filling the fields', function() {
+
+    // Arrange:
+    browser.get('user');
+
+    // Act:
+    element(by.id('edit-submit')).click();
+
+    var errorMsg = element(by.css('.messages.error'));
+
+    // Assert:
+    expect(errorMsg.isDisplayed()).toBe(true);
+
   });
 
 });
